683ca0afb66039b13223b13d5b02abf4dd617ff8
Rickyang2233 <gefan_yang@163.com>
2024-02-22 11:40:32 +0100

    finished CTFNO1D

--- src/CTFNO.py
+++ src/CTFNO.py
@@ -1,17 +1,20 @@
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
-from einops import repeat
 
-def position_encoding(t, encoding_dim):
-    t = jnp.asarray(t) if not isinstance(t, jnp.ndarray) else t
-    t = repeat(t, '... -> ... 1')
-    pe = jnp.empty((t.shape[0], encoding_dim))
-    div_term = jnp.exp(jnp.arange(0, encoding_dim, 2) * -(jnp.log(10000.0) / encoding_dim))
-    pe = pe.at[:, 0::2].set(jnp.sin(t * div_term))
-    pe = pe.at[:, 1::2].set(jnp.cos(t * div_term))
-    return pe
+class TimeEncoding(nn.Module):
+    encoding_dim: int
+    scaling: float = 100.0
+    
+    def setup(self):
+        self.div_term = jnp.exp(jnp.arange(0, self.encoding_dim, 2) * -(jnp.log(10000.0) / self.encoding_dim))
 
+    def __call__(self, t):
+        pe = jnp.empty((len(t), self.encoding_dim))
+        factor = self.scaling * jnp.einsum('i,j->ij', t, self.div_term)
+        pe = pe.at[:, 0::2].set(jnp.sin(factor))
+        pe = pe.at[:, 1::2].set(jnp.cos(factor))
+        return pe
 
 class TMSpectralConv1D(nn.Module):
     input_dim: int
@@ -21,26 +24,21 @@ class TMSpectralConv1D(nn.Module):
 
     def setup(self):
         weights_r_shape = (self.n_modes//2+1, self.input_dim, self.output_dim)
-        weights_a_shape = (self.n_modes//2+1, self.encoding_dim)
         self.weights_r = self.param(
             'weights_r',
             nn.initializers.xavier_normal(),
             weights_r_shape
         )
-        self.weights_a = self.param(
-            'weights_a',
-            nn.initializers.xavier_normal(),
-            weights_a_shape
-        )
+        self.t_dense = nn.Dense(self.n_modes//2+1, use_bias=False, param_dtype=jnp.complex64)
 
     def __call__(self, x, t_emb):
         print(x.shape, t_emb.shape)
         x_ft = jnp.fft.rfft(x, axis=-2)
-        out_ft = jnp.zeros((x.shape[-2]//2+1, self.output_dim), dtype=jnp.complex64)
-        t_emb_transf = jnp.einsum('c,nc->n', t_emb, self.weights_a)
-        weights = jnp.einsum('n,nio->nio', t_emb_transf, self.weights_r)
-        x_ft_transf = jnp.einsum('ni,nio->no', x_ft[:self.n_modes//2+1, :], weights)
-        out_ft = out_ft.at[:self.n_modes//2+1, :].set(x_ft_transf)
+        out_ft = jnp.zeros((*x.shape[:-2], x.shape[-2]//2+1, self.output_dim), dtype=jnp.complex64)
+        t_emb_transf = self.t_dense(t_emb)
+        weights = jnp.einsum('...n,nio->...nio', t_emb_transf, self.weights_r)
+        x_ft_transf = jnp.einsum('...ni,...nio->...no', x_ft[..., :self.n_modes//2+1, :], weights)
+        out_ft = out_ft.at[..., :self.n_modes//2+1, :].set(x_ft_transf)
         out = jnp.fft.irfft(out_ft, axis=-2)
         return out
     
@@ -69,5 +67,34 @@ class TMFourierLayer1D(nn.Module):
         )
     
     def __call__(self, x, t_emb):
-        x_res = self.t_dense(t_emb) * x
+        t_emb = self.t_dense(t_emb)
+        x_res = jnp.einsum('...d,...nd->...nd', t_emb, x)
         return self.activation(self.spectral_conv(x, t_emb) + self.residual_transf(x_res))
+
+class TMFNO1D(nn.Module):
+    output_dim: int
+    lifting_dims: list
+    max_n_modes: list
+    encoding_dim: int
+    activation: nn.activation = nn.relu
+
+    def setup(self):
+        self.time_encoding = TimeEncoding(self.encoding_dim)
+        self.lifting_layer = nn.Dense(self.lifting_dims[0])
+        self.fourier_layers = [
+            TMFourierLayer1D(
+                self.lifting_dims[i],
+                self.lifting_dims[i+1],
+                self.encoding_dim,
+                n_modes=self.max_n_modes[i],
+                activation=self.activation
+            ) for i in range(len(self.lifting_dims)-1)
+        ]
+        self.projection_layer = nn.Dense(self.output_dim)
+
+    def __call__(self, x, t):
+        t_emb = self.time_encoding(t)
+        x = self.lifting_layer(x)
+        for layer in self.fourier_layers:
+            x = layer(x, t_emb)
+        return self.projection_layer(x)
